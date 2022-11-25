e2a2d503d6bd2c1d7a922dc09fd43135a791d974
Septian Maulana <septianm028@gmail.com>
2022-11-25 13:14:06 +0700

    build: set config for eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  ignorePatterns: ['bin/*'],
+  rules: {},
+};
